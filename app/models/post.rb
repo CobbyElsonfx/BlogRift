97b855d59e2d5b96b243504f10defbfbfd532d7b
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-11-24 20:43:06 +0000

    added methods for recents post and updating counters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,21 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :comments
-    has_many :likes
-  
-    validates :title, presence: true
-    validates :text, presence: true
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  validates :title, presence: true
+  validates :text, presence: true
+
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
   end
-  
+
+  def update_comments_counter
+    update(comments_counter: comments.count)
+  end
+
+  def update_likes_counter
+    update(likes_counter: likes.count)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
-class User <  ApplicationRecord
-    has_many :posts , foreign_key: :author_id
-    has_many :comments
-    has_many :likes
+class User < ApplicationRecord
+  has_many :posts, foreign_key: :author_id
+  has_many :comments
+  has_many :likes
 
-    validates :name, presence: true
-    validates :photo, presence: true
-    validates :bio, presence: true    
+  validates :name, presence: true
+  validates :photo, presence: true
+  validates :bio, presence: true
 
+  def recent_posts(limit = 3)
+    posts.order(created_at: :desc).limit(limit)
+  end
 
+  def update_posts_counter
+    update(posts_counter: posts.count)
+  end
 end
