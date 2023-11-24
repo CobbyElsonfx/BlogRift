9b6d386b787cf5315a2c9275944b7d268f73a836
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-11-24 10:54:58 +0000

    moved linters.yml file

--- db/schema.rb
+++ db/schema.rb
@@ -5,29 +5,27 @@
 
 # frozen_string_literal: true
 
-
 ActiveRecord::Schema[7.1].define(version: 20_231_124_074_659) do
-  # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
   create_table 'comments', force: :cascade do |t|
     t.bigint 'user_id', null: false
     t.bigint 'post_id', null: false
     t.text 'text'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['post_id'], name: 'index_comments_on_post_id'
-    t.index ['user_id'], name: 'index_comments_on_user_id'
+    t.timestamps null: false
   end
+
+  add_index 'comments', ['post_id'], name: 'index_comments_on_post_id'
+  add_index 'comments', ['user_id'], name: 'index_comments_on_user_id'
 
   create_table 'likes', force: :cascade do |t|
     t.bigint 'user_id', null: false
     t.bigint 'post_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['post_id'], name: 'index_likes_on_post_id'
-    t.index ['user_id'], name: 'index_likes_on_user_id'
+    t.timestamps null: false
   end
+
+  add_index 'likes', ['post_id'], name: 'index_likes_on_post_id'
+  add_index 'likes', ['user_id'], name: 'index_likes_on_user_id'
 
   create_table 'posts', force: :cascade do |t|
     t.bigint 'author_id'
@@ -35,20 +33,20 @@ ActiveRecord::Schema[7.1].define(version: 20_231_124_074_659) do
     t.text 'text'
     t.integer 'comments_counter'
     t.integer 'likes_counter'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['author_id'], name: 'index_posts_on_author_id'
+    t.timestamps null: false
   end
+
+  add_index 'posts', ['author_id'], name: 'index_posts_on_author_id'
 
   create_table 'users', force: :cascade do |t|
     t.string 'name'
     t.string 'photo'
     t.text 'bio'
     t.integer 'posts_counter'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['name'], name: 'index_users_on_name'
+    t.timestamps null: false
   end
+
+  add_index 'users', ['name'], name: 'index_users_on_name'
 
   add_foreign_key 'comments', 'posts'
   add_foreign_key 'comments', 'users'
