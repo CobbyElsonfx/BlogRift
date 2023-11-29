1143a2d662f301019a10c9cf63c7ce0dee477510
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-11-29 14:23:14 +0000

    added routes and controllers for individual models

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+class PostsController 
+    before_action: set_user
+    def index
+        @posts = @user.posts
+    end 
+
+    def show
+        @post = @user.posts.find(params[id])
+    end 
+
+    def set_user 
+        @user = User.find(params[:id])
+    end
+end
