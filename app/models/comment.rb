88480c0a988820e1a1308dd34a7b985a92b96edb
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-12-02 08:55:56 +0000

    resolved stylint error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-
   after_save :update_post_comments_counter
 
   def update_post_comments_counter
