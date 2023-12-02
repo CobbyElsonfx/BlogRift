d324750502a3ae273fe2a95ce48e3aff238d4d2b
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-12-02 23:19:02 +0000

    added forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
+# ApplicationController.rb
 class ApplicationController < ActionController::Base
+  def current_user
+    # For simplicity, returning the first user from the database.
+    # In a real-world scenario, you would implement proper authentication logic.
+    @current_user = User.first
+  end
 end
