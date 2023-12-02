b9bc08157391809b9a2eacca155e06d574b64560
MR. FRANCIS ANDOH <andohfrancis9187@gmail.com>
2023-12-02 09:18:44 +0000

    removed uneccessary comments

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -1,28 +1,4 @@
-# module Users
-#   class PostsController < ApplicationController
-#     before_action :set_user
-#     before_action :set_post, only: [:show]
-
-#     def index
-#       @posts = @user.posts
-#     end
-
-#     def show
-#       @top_comments = @post.top_comments
-#     end
-
-#     private
-
-#     def set_user
-#       @user = User.find(params[:user_id])
-#     end
-
-#     def set_post
-#       @post = @user.posts.find(params[:id])
-#     end
-#   end
-# end
-
+module Users
 class Users::PostsController < ApplicationController
   before_action :set_user
   before_action :set_post, only: [:show]
