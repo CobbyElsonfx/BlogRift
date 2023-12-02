82dcf478176b84dbe799066b29c909e20ac284b1
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-12-02 01:43:22 +0000

    added views

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -1,24 +1,45 @@
-module Users
-  class PostsController < ApplicationController
-    before_action :set_user
-    before_action :set_post, only: [:show]
+# module Users
+#   class PostsController < ApplicationController
+#     before_action :set_user
+#     before_action :set_post, only: [:show]
 
-    def index
-      @posts = @user.posts
-    end
+#     def index
+#       @posts = @user.posts
+#     end
 
-    def show
-      @top_comments = @post.top_comments
-    end
+#     def show
+#       @top_comments = @post.top_comments
+#     end
 
-    private
+#     private
 
-    def set_user
-      @user = User.find(params[:user_id])
-    end
+#     def set_user
+#       @user = User.find(params[:user_id])
+#     end
 
-    def set_post
-      @post = @user.posts.find(params[:id])
-    end
+#     def set_post
+#       @post = @user.posts.find(params[:id])
+#     end
+#   end
+# end
+
+class Users::PostsController < ApplicationController
+  before_action :set_user
+  before_action :set_post, only: [:show]
+
+  def show
+    @top_comments = @post.top_comments
+    @author = @post.author
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_post
+    @post = @user.posts.includes(:author, :comments).find(params[:id])
   end
 end
+
